e1de043696e5cb545830fd381d6c1f42640ea02a
leonard315 <alindatoleonard619@gmail.com>
2025-10-19 02:43:00 +0800

    Update navbar.jsx

--- src/assets/components/ui/navbar.jsx
+++ src/assets/components/ui/navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'; // Assuming you're using react-router f
 const NavBar = () => {
   return (
     <nav className="w-full flex justify-between items-center px-6 md:px-12 lg:px-20 xl:px-32 py-5 bg-white shadow-sm">
-      <Link to="/" className="text-2xl font-bold text-indigo-600">RC CarDeal</Link>
+      <Link to="/" className="text-2xl font-bold text-indigo-600">CarDeal</Link>
       <div className="space-x-8 hidden md:flex items-center">
         <Link to="/" className="text-gray-700 hover:text-indigo-600 font-medium">Home</Link>
         <Link to="/listing" className="text-gray-700 hover:text-indigo-600 font-medium">Cars</Link>
